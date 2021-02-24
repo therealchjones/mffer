7a37e2d357523faea1cf9a3cc1992a2027b01561
Christian Jones <chjones@aleph0.com>
2021-02-24 07:20:21 -0500

    More simple formatting, VS Code settings

--- Player Classes.cs
+++ Player Classes.cs
@@ -2,13 +2,13 @@ using System.Collections.Generic;
 
 namespace MFFDataApp
 {
-    public class Player : Component
-    {
-        public Alliance alliance { get; set; }
-        public List<MyCharacter> MyRoster { get; set; }
-    }
-    public class MyCharacter
-    {
-        public Character BaseCharacter { get; set; }
-    }
+	public class Player : Component
+	{
+		public Alliance alliance { get; set; }
+		public List<MyCharacter> MyRoster { get; set; }
+	}
+	public class MyCharacter
+	{
+		public Character BaseCharacter { get; set; }
+	}
 }
